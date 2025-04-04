45e31660d4741cfa395ec9a6470bb89f72fafd7e
Maks Shkafar <m.shkafar@gmail.com>
2025-04-04 21:18:21 +0200

    add funcs to class Record

--- hm_6.py
+++ hm_6.py
@@ -16,3 +16,16 @@ class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+
+        def add_phone(self, phone: str):
+            self.phones.append(Phone(phone))
+
+        def remove_phone(self, phone: str):
+            self.phones = [p for p in self.phones if p.value != phone]
+
+        def edit_phone(self, old_phone: str, new_phone: str):
+            for phone in self.phones:
+                if phone.value == old_phone:
+                    phone.value = new_phone
+                    return
+            raise ValueError("Телефон не знайдений")
